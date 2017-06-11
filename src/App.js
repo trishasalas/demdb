01af366b1a309d89f3c058da3ba2b5975d22533d
Trisha Salas <trisha@trishasalas.com>
2017-06-10 23:22:35 -0500

    add yarn and postcss

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+
+const MyComponent = Vue.extend({
+  // extension options
+})
+// all instances of `MyComponent` are created with
+// the pre-defined extension options
+const myComponentInstance = new MyComponent()
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,43 @@
+const webpack = require('webpack');
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const extractStyles = new ExtractTextPlugin('app.css');
+const extractFonts = new ExtractTextPlugin('fonts.css');
 
 module.exports = {
-  entry: './src/app.js',
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    app: './App.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /App\.css/,
+        loader: extractStyles.extract({
+          loader: 'css-loader?importLoaders=1!postcss-loader',
+        }),
+      },
+      {
+        test: /Font\.css/,
+        loader: extractFonts.extract({
+          loader: 'css-loader',
+        }),
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: ['url-loader'],
+      },
+    ],
+  },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    path: path.resolve(__dirname, 'dist/assets'),
+    filename: '[name].bundle.js',
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+  },
+  plugins: [
+    extractStyles,
+    extractFonts,
+  ],
 };
